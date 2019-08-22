1e2ced8db02109bc3d559efef333bdee83eb6fc2
gyj <1401357358@qq.com>
2019-08-22 13:16:43 +0800

    分页不过处理，没有数据还会发送请求，网速慢时，一次请求可能发送多次。

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     list: [],
-    screenHeight:""
+    screenHeight:"",
+    page:1,
+    //flag标识
+    flag:true
   },
 
   /**
@@ -17,7 +20,6 @@ Page({
     //获得屏幕高度
     wx.getSystemInfo({
       success: (res)=> {
-
         this.setData({
           screenHeight:res.windowHeight
         })
@@ -26,78 +28,61 @@ Page({
   },
   //加载数据列表
   getList(){
+    this.setData({
+      flag:false
+    })
     var api = 'http://www.phonegap100.com/appapi.php'
     wx.request({
       url: api,
       data:{
         a: 'getPortalList',
         catid:'20',
-        page:1
+        page:this.data.page
       },
       success:(res)=>{
-       this.setData({
-         list:res.data
+        //判断是否是最后一页数据，
+        if(res.data.result.length<20){
+          //是最后一页，将flag设为false。
+          var f=false
+        }
+        else{
+          var f=true
+        }
+        //每页加载的数据进行拼接
+        let list=this.data.list.concat(res.data.result)
+
+        //页数自增。
+        let page=++this.data.page
+        this.setData({
+         list:list,
+         page:page,
+         flag:f
+        
        })
       }
     })
+    
   },
+  //分页：加载更多
+  //页面滑动到底端时触发此方法。
+  loadMore() {
+    //调用函数
+    if(this.data.flag){
+      //必须flag为true时调用，因为有可能网速慢时，一次getlist还没完成，
+      //已经触发loadmore进行第二次了。但是getlist代码没执行完，所以会请求多次。
+      this.getList()
+    }
+   
+
+  },
+
   //跳转到详情页
   toDetail(e){
     let aid=e.currentTarget.dataset.aid
    wx.navigateTo({
      url: "/pages/indexDetail/indexDetail?aid="+aid,
    })
-  },
-  //加载更多
-  loadMore(){
-    console.log("more")
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
+
+
 })
